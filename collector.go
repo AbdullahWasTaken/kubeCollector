f23817324dfc4bc095bae990457c57e5d740d018
Abdullah Khan <abdullah.khan4620@gmail.com>
2022-09-25 02:36:21 -0400

    Using dynamic client

--- collector.go
+++ collector.go
@@ -3,81 +3,62 @@ package collector
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"
+	"fmt"
 	"os"
 	"path/filepath"
-	"reflect"
 
+	"github.com/AbdullahWasTaken/kube-miner/config"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/exp/slices"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 )
 
-func Workflow() {
-	///////////////////////////////////////////////////////////
-	//					CONFIGURATION						 //
-	///////////////////////////////////////////////////////////
-	home := homedir.HomeDir()
-	var kubeconfig string = filepath.Join(home, ".kube", "config")
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+// GetClusterState is a naive implementation of operational state extraction from a kubernetes cluster.
+func GetClusterState(cfg *config.Config) {
+	groupList, err := cfg.DisClient.ServerGroups()
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
+	// Iterating over groups supported by the server.
+	for _, group := range groupList.Groups {
+		// Iterating over all versions (may not be preferred) supported by the server.
+		for _, gvs := range group.Versions {
+			apiResourceList, err := cfg.DisClient.ServerResourcesForGroupVersion(gvs.GroupVersion)
+			if err != nil {
+				log.Error(err)
+			}
+			// Iterating over the resources present under a particular group and version.
+			for _, apiRes := range apiResourceList.APIResources {
+				// schmea.GroupVersionResource is uniquely defined by Group, Version, and ResourceName.
+				gvr := schema.GroupVersionResource{Group: group.Name, Version: gvs.Version, Resource: apiRes.Name}
+				// pruning out resources that do not support LIST action.
+				if slices.Contains(apiRes.Verbs, "list") {
+					GetResources(*cfg.DynClient, gvr, cfg.Out)
+				}
+			}
+		}
 	}
-	///////////////////////////////////////////////////////////
-	//						QUERYING						 //
-	///////////////////////////////////////////////////////////
-	const resources string = "pods"    // check if valid and supported
-	const namespace string = "default" // None->all namespaces, ""-> default
-
-	// client := clientset.CoreV1().RESTClient()
-	client := getClient(clientset, resources)
-
-	// var reply rest.Result = client.Get().
-	// 	Resource(resources).
-	// 	Namespace(namespace).
-	// 	VersionedParams(&metav1.ListOptions{}, scheme.ParameterCodec).
-	// 	Do(context.Background())
-
-	var reply rest.Result = query(client, resources, namespace)
-
-	///////////////////////////////////////////////////////////
-	// 						DECODING						 //
-	///////////////////////////////////////////////////////////
-	runObj, err := reply.Get() // decode the runtime.Object
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// Output Format: JSON
-	b, err := json.MarshalIndent(runObj, "", "  ")
-	if err != nil {
-		panic(err.Error())
-	}
-	// Output Mode: stdout, file, dgraph
-	_ = ioutil.WriteFile("result.json", b, os.ModePerm)
 }
 
-func getClient(clientset *kubernetes.Clientset, resource string) rest.Interface {
-	var v reflect.Value = reflect.ValueOf(clientset)
-	const api = "CoreV1"
-	const restClient = "RESTClient"
-	var result rest.Interface = v.MethodByName(api).Call([]reflect.Value{})[0].MethodByName(restClient).Call([]reflect.Value{})[0].Interface().(rest.Interface)
-	return result
-}
+// GetResources performs the LIST action over a valid scheme.GroupVersionResource
+// iterating over each resource present and encoding the Unstructured.Unstructured object into JSON
+func GetResources(client dynamic.Interface, gvr schema.GroupVersionResource, path string) {
+	resourcelist, err := client.Resource(gvr).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Error(err)
+		return
+	}
 
-func query(client rest.Interface, resource, namespace string) rest.Result {
-	var req *rest.Request = client.Get()
-	req = req.Resource(resource)
-	req = req.Namespace(namespace)
-	res := req.VersionedParams(&metav1.ListOptions{}, scheme.ParameterCodec).Do(context.Background())
-	return res
+	// creating the output path if it doesn't exists.
+	err = os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if numRes := len(resourcelist.Items); numRes > 0 {
+		log.Infof("Found %v instances of resource type %v\n", numRes, gvr.Resource)
+		b, _ := json.MarshalIndent(resourcelist.Items, "", "    ")
+		os.WriteFile(filepath.Join(path, fmt.Sprintf("%v.json", gvr.Resource)), b, os.ModePerm)
+	}
 }

1f7d5d40c1a31ac80f00336a359889e0cb55a6d9
revelo <abdullah.khan4620@gmail.com>
2022-10-22 20:44:22 -0400

    save only

--- collector.go
+++ collector.go
@@ -18,27 +18,24 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// Collector represents the state of a kubernetes system accessed through config
+// Collector represents a kubernetes system accessible through config
 type Collector struct {
-	state  map[schema.GroupVersionResource]unstructured.UnstructuredList
 	config *rest.Config
 }
 
 // NewCollector creates a new Collector instance
 // and build the config object from kubeconfig path.
 func NewCollector(kubeconfig string) *Collector {
-	st := map[schema.GroupVersionResource]unstructured.UnstructuredList{}
 	conf, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return &Collector{state: st, config: conf}
+	return &Collector{config: conf}
 }
 
-// Collect is responsible for querying the Kubernetes API server
-// for the states of all available resources.
-func (c *Collector) Collect() (cs map[schema.GroupVersionResource]unstructured.UnstructuredList) {
-	cs = map[schema.GroupVersionResource]unstructured.UnstructuredList{}
+// Collect queries the Kubernetes API server for the states of all available resources
+// and saves them individually in a subdirectory called JSON under `outDir`.
+func (c *Collector) Collect(outDir string) {
 	dc, err := dynamic.NewForConfig(c.config)
 	if err != nil {
 		log.Error(err)
@@ -47,38 +44,42 @@ func (c *Collector) Collect() (cs map[schema.GroupVersionResource]unstructured.U
 	if err != nil {
 		log.Error(err)
 	}
+	jsonPath := filepath.Join(outDir, "JSON")
+	err = os.MkdirAll(jsonPath, os.ModePerm)
+	if err != nil {
+		log.Panic(err)
+	}
+	// get a list of resoirces to query
 	res, _ := getResources(tc.DiscoveryClient)
 	for _, gvr := range res {
+		// get resource state
 		st, err := getResourceState(dc, gvr)
 		if err != nil {
 			log.Error(err)
 		} else {
 			if len(st.Items) > 0 {
-				cs[gvr] = st
-			}
-		}
-	}
-	return cs
-}
-
-// SaveState converts the state of the Collector into JSON format and
-// stores it individually in a directory under `outDir` called JSON.
-func (c *Collector) SaveState(outDir string) {
-	jsonPath := filepath.Join(outDir, "JSON")
-	err := os.MkdirAll(jsonPath, os.ModePerm)
-	if err != nil {
-		log.Panic(err)
-	}
-	for gvr, st := range c.state {
-		// convert Unstructured.Undtructred to JSON
-		jsonStr, err := json.MarshalIndent(st, "", "    ")
-		if err != nil {
-			log.Error(err)
-		} else {
-			// write JSON to file
-			err = os.WriteFile(filepath.Join(jsonPath, gvr.Resource+".json"), jsonStr, os.ModePerm)
-			if err != nil {
-				log.Error(err)
+				// convert Unstructured.Undtructred to JSON
+				jsonStr, err := json.MarshalIndent(st, "", "    ")
+				if err != nil {
+					log.Error(err)
+				} else {
+					// write JSON to file
+					sp := filepath.Join(jsonPath, gvr.Resource+".json")
+					fp, err := os.OpenFile(sp, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+					// err = os.WriteFile(f, jsonStr, fs.FileMode(os.O_CREATE|os.O_APPEND))
+					if err != nil {
+						log.Error(err)
+					} else {
+						defer fp.Close()
+						n, err := fp.Write(jsonStr)
+						if err != nil {
+							log.Error(err)
+						}
+						if n != len(jsonStr) {
+							log.Error("Write failed on ", fp.Name())
+						}
+					}
+				}
 			}
 		}
 	}
